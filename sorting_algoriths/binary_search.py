3b7bbd33dbf64534a683bed6fbf471cee1ba8d43
Costeira <joaocurveira13@gmail.com>
2022-10-27 12:27:33 +0100

    binary search

---
+++ sorting_algoriths/binary_search.py
@@ -0,0 +1,22 @@
+def binary_search(list, item):
+    
+    low = 0
+    high = len(list)  - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+
+        if guess == item:
+            return mid 
+        else:
+            if guess > item:
+                high = mid - 1
+            else:
+                low = mid + 1
+    return  None
+
+
+
+
+print(binary_search(list(range(0,1000,10)), 500))
